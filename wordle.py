9b4f363284a9be153fa7716450d1825afbf066c6
Geoff Briggs <geoff.briggs@cantab.net>
2022-01-18 23:21:31 +0000

    Better handling of yellow clues

--- wordle.py
+++ wordle.py
@@ -11,10 +11,10 @@ import string
 
 def usage():
     progname = sys.argv[0]
-    message = '''
+    message = f'''
 Wordle Hinter: find words that might fit a Wordle puzzle.
 
-Usage:"
+Usage:
     {progname} -g <greenpattern> -y <yellowchars> -e <eliminated> [-p] [-l wordlist] [-n N] [-f] [-s]
 
 <greenpattern> should use dots for unknowns, and letters where known, e.g. '..B.Y'
@@ -42,34 +42,51 @@ Examples:
     {progname} -g '...RT' -p | column
     {progname} -g '.AIN.' -y 'T' -e 'S'
     {progname} -l mywordlist.txt -f
+'''
 
-    '''
     print(message, file=sys.stderr)
     sys.exit(1)
 
 # ------- Part 1: Find possible words ---------
 
 # Function to turn known letters into a compiled regex for word matching/elimination
-def buildRegex(green, yellow, grey):
-    if len(green) != 5:
-        print("\nFATAL: I only work on five-letter words. The green pattern must be of length 5 (or absent).\n", file=sys.stderr)
-        usage()
-    yellowregex=""
-    greyregex=""
-    for char in yellow:
-        yellowregex += "(?![^" + char + "]{5})"                     # Yellow means we know a character is present, so each regex says "must not be a string containing five characters that aren't this one"
-    if len(grey)>0:
-        greyregex = "(?!.*["+grey+"].*)"                            # Grey means the character isn't present, so this regex says "must not be a string containing any character in this list"
-    # Compile regexes to implement inputs
-    regexstr = '^'+yellowregex+greyregex+green+'$'
-    return re.compile(regexstr)
+def buildRegex(known):
+    green="....."
+    yellow=""
+    grey=""
+    regex=""
+    for clue in known:
+        if not re.match('^(g[1-5][a-z]|y[1-5][a-z]|x[a-z])$', clue):
+            usage()
+        
+        if clue[0] == 'g':
+            pos = int(clue[1]) - 1    # Convert position identifier to zero-index
+            green = green[:pos] + clue[2] + green[pos+1:]
+        
+        elif clue[0] == 'y':
+            pos = int(clue[1]) - 1    # Convert position identifier to zero-index
+            yellow += "(?=.*" + clue[2] + ")(?!" + ("."*pos) + clue[2] + ("."*(4-pos)) + ")"
+        
+        elif clue[0] == 'x':
+            grey += "(?!.*" + clue[1] + ")"
+        else:
+            usage()     # Shouldn't really be possible to get here
 
-# Subroutine to filter (in place) a list of words, using the supplied regex
-def filterWords(wlist, regex):
-    for word in tuple(wlist):                                       # Need to create a copy of wlist, as we're modifying it place. Can't iterate over it and modify it at the same time, we end up shooting at a moving target.
-        if not regex.match(word):
-            wlist.remove(word)
-#    return wlist                                                    # Not necessary, as wlist is passed in by reference anyway, but harmless
+    regex = "^" + yellow + grey + green
+    # print("Debug: regex is", regex)
+    return re.compile(regex)
+
+# Subroutine to filter (in place) a list of words, using the supplied regex, optionally just returning the number of words that would have been removed without actually doing it
+def filterWords(words, regex, justCount=False):
+    killlist = []
+    for i in range(len(words)):
+        if not regex.match(words[i]):
+            killlist.append(i)
+    if not justCount:
+        killlist.reverse()
+        for i in killlist:
+            del(words[i])
+    return len(killlist)
 
 # Function to read a file into a list
 def readListFromFile(wordlist_filename):
@@ -85,16 +102,22 @@ def readListFromFile(wordlist_filename):
     return words
 
 # Method 1 for finding a good next guess
-def method1(words, green):
+def method1(words, known):
     # When converting to a score, the aim is to bisect the option space, so closest to 50% match is best
     goal = len(words)/2
 
-    # Find letter frequencies in the un-fixed positions
-    positions={}
-    for pos in range(5):
-        if not green[pos]==".":
-            continue
+    # Which letters are known vs unknown? Need to pull out the greens from our clues
+    unfixed = [i for i in range(5)]
+    for clue in known:
+        if re.match('^g[1-5][a-z]$', clue) and (int(clue[1])-1) in unfixed:
+            unfixed.remove(int(clue[1]) - 1)   # Convert position identifier to zero-index
+    if len(unfixed) == 0:
+        print("Duh... wut? We already know the answer.", file=sys.stderr)
+        sys.exit(1)
 
+    # Find letter frequencies in the unfixed positions
+    positions={}
+    for pos in unfixed:
         positions[pos]={}
         for word in words:
             if not word[pos] in positions[pos]:
@@ -104,7 +127,7 @@ def method1(words, green):
 
     for pos in positions.keys():
         for char in positions[pos].keys():
-            positions[pos][char] = abs(positions[pos][char] - goal)     # Convert to distance from being a perfect bisector
+            positions[pos][char] = abs(positions[pos][char] - goal)/goal     # Normalize to distance from being a perfect bisector
 
     # Sum up the total distance for each candidate next-guess
     m1dict={}
@@ -112,13 +135,13 @@ def method1(words, green):
         score = 0
         for pos in positions.keys():
             score += positions[pos][word[pos]]
-        m1dict[word]=score
+        m1dict[word]=score/len(positions)
 
     return m1dict
 
 # Method 2 - see how a each guessword would reduce the solution space
-def method2(trylist, wordlist, origgreen, origyellow, origgrey):
-    retdict={}
+def method2(trylist, wordlist, known):
+    m2dict={}
     cnt=0
     pid=os.getpid()
     starttime=time.time()
@@ -128,59 +151,44 @@ def method2(trylist, wordlist, origgreen, origyellow, origgrey):
         guesswordscore = 0
         avetime = (time.time() - starttime)/cnt
         esttime = (len(trylist)-cnt)*avetime/60
-        # print(f"PID {pid} is on {cnt} of {len(trylist)}. Average time is {avetime:2.4f}, estimate {esttime:4.1f} minutes")
+        #print(f"PID {pid} is on {cnt} of {len(trylist)}. Average time is {avetime:2.4f}, estimate {esttime:4.1f} minutes")
 
         # One of the words in wordlist must be the true answer. Evaluate guessword against each, and see what the resulting solution space would be.
         for trueword in wordlist:
-            
+            # print("Debug: guessword",guessword,"against trueword",trueword)
 
             # Maybe we got lucky...
             if guessword == trueword:   
                 continue                    # Don't add anything to the solution space count, as we found it
 
             # If not, then the resulting pattern updates would be...
-            newgreen=origgreen
-            newyellow=origyellow
-            newgrey=origgrey
-            yellowpositions=""
-            yellowregex=""
-            greyregex=""
+            newknown = []
             for pos in range(5):
                 g = guessword[pos]
                 t = trueword[pos]
                 if g == t:
-                    newgreen = newgreen[:pos] + g + newgreen[pos+1:]
+                    newknown.append('g' + str(pos+1) + g)
                 elif g in trueword:
-                    yellowpositions += "(?!" + ("."*pos) + g + ("."*(4-pos)) + ")"
-                    if not g in newyellow:
-                        newyellow+=g
-                elif not g in trueword and not g in newgrey:
-                    newgrey+=g
-            if len(newgrey)>0:
-                greyregex = "(?!.*["+newgrey+"].*)"
-            for char in newyellow:
-                yellowregex += "(?=.*"+char+")"
-            regexstr = '^'+yellowpositions+yellowregex+greyregex+newgreen+'$'
-            #print("Guess",guessword,"True",trueword,"Regex",regexstr)
-            newregex = re.compile(regexstr)
+                    newknown.append('y' + str(pos+1) + g)
+                elif not g in trueword and not ('x'+g) in known:
+                    newknown.append('x' + g)
+            # print("Debug: new knowns are",newknown)
+            newregex = buildRegex(known+newknown)
 
             # ...and count how many words that would remove
-            kills = 0
-            for w in wordlist:
-                if not newregex.match(w):
-                    kills += 1
+            kills = filterWords(wordlist, newregex, justCount=True)
 
             # The score is the size of the remaining wordlist
-            guesswordscore += len(wordlist) - kills
-
+            guesswordscore += (len(wordlist) - kills) / len(wordlist)
+            # print("Debug: guessword",guessword,"against trueword",trueword,"scores",guesswordscore)
         
         # (end of trueword loop)
 
         # We can now calculate the average tree depth under guessword
         guesswordscore /= len(wordlist)
-        retdict[guessword]=guesswordscore
+        m2dict[guessword]=guesswordscore
 
-    return retdict
+    return m2dict
 
 # Method 3 - letter frequency analysis is fast, but can we apply it across all positions rather than treating each position separately?
 def method3(trylist, wordlist):
@@ -192,32 +200,22 @@ def method3(trylist, wordlist):
             if ch in w:
                 wordsContaining[ch] += 1
     
-    # Convert from absolute frequency to distance from 50% (aim is to bisect solution space with each letter)
+    # Convert from absolute frequency to normalized distance from 50% (aim is to bisect solution space with each letter)
     goal = len(words)/2
     for ch in wordsContaining.keys():
-        wordsContaining[ch] = abs(wordsContaining[ch] - goal)
-   
+        wordsContaining[ch] = abs(wordsContaining[ch] - goal) / goal
+
     # Add up the score for each candidate word in trylist
     m3dict={}
     for guessword in trylist:
         m3dict[guessword]=0
         for pos in range(5):
             if guessword[pos] in guessword[:pos]:
-                m3dict[guessword] += len(wordlist)          # Penalise repeated letters, as they add no new information
+                m3dict[guessword] += 1         # Penalise repeated letters, as they add no new information
             else:
-                m3dict[guessword] += wordsContaining[ch]
+                m3dict[guessword] += wordsContaining[guessword[pos]] / 5 
 
     return m3dict
-
-# #### HACKMAIN for testing
-# gwords = readListFromFile("wordlelist.txt")
-# words = list(gwords)
-# filterWords(words,buildRegex('...e.', 'a', 'sortidl'))
-# m2dict = method2(words, words, '...e.', 'a', 'sortidl')
-# m2bestwords = sorted(m2dict.items(), key=operator.itemgetter(1), reverse=False)
-# print("\nRanked options:")
-# print(m2bestwords[0:20])
-# sys.exit(0)
 
 #### MAIN
 if __name__ == '__main__':
@@ -225,26 +223,19 @@ if __name__ == '__main__':
     # Read commandline arguments
     argv = sys.argv[1:]
     try:
-        opts, args = getopt.getopt(argv, "g:y:pl:n:e:fs")
+        opts, args = getopt.gnu_getopt(argv, "pl:n:fsm")
     except:
         usage()
 
-    greenpattern = "....."    # Default: five characters, could be anything
-    yellowletters = ""
-    greyletters = ""
+    known = [x.lower() for x in args]        # Remaining options after parsing out the getopt params
     printall = False
     wordlist_filename = "wordlelist.txt"
     n = 1
     fast = True 
     slow = True 
+    multicore = False
 
     for opt,arg in opts:
-        if opt == '-g':
-            greenpattern = arg.lower()
-        if opt == '-y':
-            yellowletters = arg.lower()
-        if opt == '-e':
-            greyletters = arg.lower()
         if opt == '-p':
             printall = True
         if opt == '-l':
@@ -255,6 +246,8 @@ if __name__ == '__main__':
             slow = False
         if opt == '-s':
             fast = False
+        if opt == '-m':
+            multicore = True
 
     if not fast and not slow:
         fast = True 
@@ -262,15 +255,15 @@ if __name__ == '__main__':
 
     # Read and filter the word list, based on the supplied inputs, to get candidate words
     words = readListFromFile(wordlist_filename)
-    regex = buildRegex(greenpattern, yellowletters, greyletters)
+    regex = buildRegex(known)
     filterWords(words, regex)
 
     # Display findings
     if len(words)>20 and not printall:
-        print("There are", len(words), "candidate words")
-        print("Random word:",random.choice(words))
+        print("There are", len(words), "candidate words, including gems such as",random.choice(words),"and",random.choice(words))
     elif len(words)==0:
         print("No matches found. We're gonna need a bigger wordlist.")
+        sys.exit(0)
     else:
         print("\nCandidate words:")
         print("\n".join(words))
@@ -280,7 +273,7 @@ if __name__ == '__main__':
     if fast:
         ### Method 1: somewhat naive heuristic, aiming to hit greens (basically ignores yellows, so probably not optimal)
         m1start = time.time()
-        m1dict=method1(words, greenpattern)
+        m1dict=method1(words, known)
         m1bestwords = sorted(m1dict.items(), key=operator.itemgetter(1), reverse=False)
         m1end = time.time()
 
@@ -296,28 +289,26 @@ if __name__ == '__main__':
         m3bestwords = sorted(m3dict.items(), key=operator.itemgetter(1), reverse=False)
         m3end = time.time()
 
-        print("\nFor the next guess, method 3 suggests that you try",m3bestwords[0][0])
+        print("For the next guess, method 3 suggests that you try",m3bestwords[0][0])
         if n>1:
             print("\nOther reasonable options:")
             for w in m3bestwords[1:n]:
                 print(w[0])
     if slow:
-        ### Implementation 2: don't treat each char separately, see which word best halves the remaining solution space
         m2start = time.time()
-
-        # Initialise a dictionary to keep score
-        m2dict = {}
-        # This part can take a while, so let's play with multi-processing.
-        cores = mp.cpu_count()
-        mypool = mp.Pool(processes=cores)
-        l = len(words)
-        wordslices = [words[int(l*(i/cores)):int(l*((i+1)/cores))] for i in range(cores)]
-        results = mypool.starmap(method2, [(wordslices[i], words, greenpattern, yellowletters, greyletters) for i in range(cores)])
-        for res in results:
-            m2dict.update(res)
-
-        # # Or maybe let's just see if the old M2 still works
-        # m2dict=method2(words, words, greenpattern, yellowletters, greyletters)
+        m2dict={}        
+        
+        if multicore:
+            # This part can take a while, so let's play with multi-processing.
+            cores = mp.cpu_count()
+            mypool = mp.Pool(processes=cores)
+            l = len(words)
+            wordslices = [words[int(l*(i/cores)):int(l*((i+1)/cores))] for i in range(cores)]
+            results = mypool.starmap(method2, [(wordslices[i], words, known) for i in range(cores)])
+            for res in results:
+                m2dict.update(res)
+        else:
+            m2dict=method2(words, words, known)
 
         m2bestwords = sorted(m2dict.items(), key=operator.itemgetter(1), reverse=False)
 
@@ -330,7 +321,11 @@ if __name__ == '__main__':
                 print(w[0])
 
     if fast and slow:
-        print("SCORE COMPARISON")
-        print('Method1 recommended {0:}, scored {1:6.2f} by M1 and {2:6.2f} by M2, found in {3:6.4f} seconds.'.format(m1bestwords[0][0], m1bestwords[0][1], m2dict[m1bestwords[0][0]], m1end-m1start))
-        print('Method2 recommended {0:}, scored {1:6.2f} by M1 and {2:6.2f} by M2, found in {3:6.4f} seconds.'.format(m2bestwords[0][0], m1dict[m2bestwords[0][0]], m2bestwords[0][1], m2end-m2start))
+        print("\nSCORE COMPARISON")
+        m1=m1bestwords[0][0]
+        m2=m2bestwords[0][0]
+        m3=m3bestwords[0][0]
+        print('Method1 top {0:}, scores M1: {1:4.2f} M2: {2:4.2f} M3: {3:4.2f}, found in {4:6.4f} seconds.'.format(m1, m1dict[m1], m2dict[m1], m3dict[m1], m1end-m1start))
+        print('Method2 top {0:}, scores M1: {1:4.2f} M2: {2:4.2f} M3: {3:4.2f}, found in {4:6.4f} seconds.'.format(m2, m1dict[m2], m2dict[m2], m3dict[m2], m2end-m2start))
+        print('Method3 top {0:}, scores M1: {1:4.2f} M2: {2:4.2f} M3: {3:4.2f}, found in {4:6.4f} seconds.'.format(m3, m1dict[m3], m2dict[m3], m3dict[m3], m3end-m3start))
 
